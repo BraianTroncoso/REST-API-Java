07efdd76663497d708b8cb9324e10fdc3c3591df
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-03-06 22:24:44 -0300

    [ADD]Reemplazo de archivos 1.0

--- src/main/java/com/alienexplorer/app/rest/Repository/TodoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.alienexplorer.app.rest.Repository;
-
-import com.alienexplorer.app.rest.Model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface TodoRepository extends JpaRepository<Task, Long> {
-}
